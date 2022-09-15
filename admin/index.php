136bc884e741684bfb95ead0b99fca2487073911
samirbaniya <samirbaniya500@gmail.com>
2022-09-15 21:55:03 +0545

    project

---
+++ admin/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/admin.css">
+    <title>Admin Panel</title>
+</head>
+<body>
+    <!-- Menu Section Starts -->
+    <section class="menubar">
+        <div class="wrapper">
+            <div class="logo">
+                <img src="../images/hamroSekuwaLogo.png" class="img-responsive" alt="logo">
+            </div>
+            <div class="menu text-right">
+                <ul>
+                    <li><a href="#">Dashboard</a></li>
+                    <li><a href="#">Category</a></li>
+                    <li><a href="#">Product</a></li>
+                </ul>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+
+    </section>
+    <!-- Menu Section Ends -->
+
+    <!-- Body Section Starts -->
+    <section class="content">
+        <div class="wrapper">
+            <div class="breadcumb menu text-center">
+                <ul>
+                    <li><a href="#">Dashboard</a></li>
+                </ul>
+            </div>
+            <div class="form-wrapper">
+                <form action="">
+                    
+                </form>
+            </div>
+        </div>
+    </section>
+    <!-- Body Section Ends -->
+
+    <!-- Footer Section Starts -->
+    <section class="Footer">
+        <div class="wrapper text-center">
+            <p>Copyright <?php echo date('Y'); ?>.</p>
+            <p> All Rights Reserves. Designed and Developed by <a href="#">Samir Baniya</a></p>
+        </div>
+    </section>
+    <!-- Footer Section Ends -->
+</body>
+</html> 
